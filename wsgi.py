dc67e84f335553866f7b40d81565f40a7a681f4e
Arthur Schneider <shitler25oki@yahoo.com>
2018-02-08 18:08:22 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import request
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    response = request.headers
+    return "request.headers"
 
 if __name__ == "__main__":
     application.run()
